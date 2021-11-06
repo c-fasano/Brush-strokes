573d460b316a50e3c099ccfaa86f90cc95e9805d
Cole-Fasano <cole.fasano@outlook.com>
2021-11-06 10:53:42 -0500

    create artwork model and embed in artist

--- models/artist.js
+++ models/artist.js
@@ -2,6 +2,14 @@ import mongoose from "mongoose"
 
 const Schema = mongoose.Schema
 
+const artworkSchema = new Schema ({ 
+  titleOfPiece: String,
+  size: Number,
+  medium: String,
+  Surface: String,
+  //comments: [{type: Schema.Types.ObjectId, ref: "Comment"}]
+})
+
 const artistSchema = new Schema ({
   name: String,
   age: Number,
